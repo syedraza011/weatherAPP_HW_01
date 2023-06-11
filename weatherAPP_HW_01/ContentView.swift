58cdc2aaf6064f7933805a9003e0b78034a46d77
Syed Raza <syed.raza011@gmail.com>
2023-06-11 17:48:06 -0400

    Initial Commit

---
+++ weatherAPP_HW_01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  weatherAPP_HW_01
+//
+//  Created by Syed Raza on 6/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherAPP_HW_01/weatherAPP_HW_01App.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAPP_HW_01App.swift
+//  weatherAPP_HW_01
+//
+//  Created by Syed Raza on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAPP_HW_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
