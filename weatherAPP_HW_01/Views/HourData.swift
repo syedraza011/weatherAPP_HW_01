bd61f875b20de7217e5b4b2abeb25f8c98d09ebb
Syed Raza <syed.raza011@gmail.com>
2023-08-08 22:21:59 -0400

    minor updates

--- weatherAPP_HW_01/Views/HourData.swift
+++ weatherAPP_HW_01/Views/HourData.swift
@@ -45,44 +45,7 @@ struct HourData: View {
            }
     
        }
-//        ScrollView(.horizontal, showsIndicators: false) {
-//            VStack{
-//                Text("Sunny conditons will continue all day.")
-//                    .foregroundColor(.white)
-//                    .background(Color.blue.opacity(0.8))
-//
-//                HStack {
-//
-//                    ForEach(HourlyData.mockData) { hour in
-//                        VStack() {
-//
-//                            Text(hour.time) // Display "9am" instead of just "9"
-//                                .frame(width: 60, height: 30) // Adjust the frame width to accommodate the text
-//                                .font(.headline)
-//                                .foregroundColor(.white)
-//
-//                            Image(systemName: hour.image)
-//                                .foregroundColor(.yellow)
-//
-//                            Text(hour.temperature)
-//                                .font(.subheadline)
-//                                .foregroundColor(.white)
-//                        }
-//                    }
-//
-//                }
-//            }
-//            .padding(.leading, 10)
-//            .background(Color.blue.opacity(0.8))
-//
-//        }
-//        .cornerRadius(15)
-//        .padding([.leading, .trailing], 20)
-//           .frame(maxWidth: .infinity)
-//
-//    }
-//
-//}
+
 
 struct HourData_Previews: PreviewProvider {
     static var previews: some View {
